7cf369d615944be23ea2f62c6b170986b85a96c9
Pankaj Jha <pankajjha@tiladmins-MacBook-Pro-10.local>
2015-02-13 12:50:37 +0530

    Initial Commit

---
+++ SampleFoundation/SampleFoundation.h
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.h
+//  SampleFoundation
+//
+//  Created by Pankaj Jha on 13/02/15.
+//  Copyright (c) 2015 Times Internet Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFoundation : NSObject
+
+@end
---
+++ SampleFoundation/SampleFoundation.m
@@ -0,0 +1,13 @@
+//
+//  SampleFoundation.m
+//  SampleFoundation
+//
+//  Created by Pankaj Jha on 13/02/15.
+//  Copyright (c) 2015 Times Internet Limited. All rights reserved.
+//
+
+#import "SampleFoundation.h"
+
+@implementation SampleFoundation
+
+@end
